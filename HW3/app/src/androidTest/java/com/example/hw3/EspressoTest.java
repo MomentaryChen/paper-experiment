2d5fe545039ddb0f431d41c05bad6f0dd0daade6
Momentary <zzser15963@gmail.com>
2020-04-15 15:02:21 +0800

    Copy HW2 Unit test and HW2 UI test, Successful

---
+++ HW3/app/src/androidTest/java/com/example/hw3/EspressoTest.java
@@ -0,0 +1,84 @@
+package com.example.hw3;
+
+import android.content.Context;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.squareup.spoon.Spoon;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class EspressoTest {
+    @Rule
+    public ActivityTestRule<RegisterActivity> mActivityRule = new ActivityTestRule(RegisterActivity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.hw3", appContext.getPackageName());
+    }
+
+    @Test
+    public void clickRegister(){
+        TextView text= (TextView) mActivityRule.getActivity().findViewById(R.id.feedbackTextView);
+        Spoon.screenshot( mActivityRule.getActivity(), "PreClickRegister");
+        onView(withId(R.id.register)) // Matcher
+                .perform(click()); // Action
+
+        Spoon.screenshot( mActivityRule.getActivity(), "AfterClickRegister");
+        assertEquals(text.getText().toString(), "Account cannot be empty.");
+    }
+    @Test
+    public void inputAccountAndClickRegister(){
+        TextView text= (TextView) mActivityRule.getActivity().findViewById(R.id.feedbackTextView);
+
+        onView(withId(R.id.editAccount))
+                .perform(typeText("zxcv1234@gmail.com"),closeSoftKeyboard());
+
+        Spoon.screenshot( mActivityRule.getActivity(), "PreInputAccount");
+
+        onView(withId(R.id.register)) // Matcher
+                .perform(click()); // Action
+
+        Spoon.screenshot( mActivityRule.getActivity(), "AfterInputAccount");
+        assertEquals(text.getText().toString(), "Password cannot be empty.");
+    }
+
+    @Test
+    public void inputPasswordAndClickRegister(){
+        TextView text= (TextView) mActivityRule.getActivity().findViewById(R.id.feedbackTextView);
+        Spoon.screenshot( mActivityRule.getActivity(), "PreInputAccount");
+
+        onView(withId(R.id.editAccount))
+                .perform(typeText("zxcv1234@gmail.com"),closeSoftKeyboard());
+        onView(withId(R.id.editPassword))
+                .perform(typeText("zxcv4561234"),closeSoftKeyboard());
+        EditText password = (EditText)mActivityRule.getActivity().findViewById(R.id.editPassword);
+
+        onView(withId(R.id.register)) // Matcher
+                .perform(click()); // Action
+
+        Spoon.screenshot( mActivityRule.getActivity(), "AfterInputAccount");
+        assertEquals(text.getText().toString(), "Password need include a capital English or more");
+    }
+}
---
+++ HW3/app/src/test/java/com/example/hw3/CheckerTest.java
@@ -0,0 +1,59 @@
+package com.example.hw3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class CheckerTest {
+    Checker checker = new Checker();
+    @Test
+    public void accountIsNull() {
+        AccountState as = checker.checkAccount("");
+        assertEquals(as, AccountState.IS_NULL);
+    }
+    @Test
+    public void accountIsNotEmail() {
+        AccountState as = checker.checkAccount("zxcv1234");
+        assertEquals(as, AccountState.IS_NOT_EMAIL);
+    }
+    @Test
+    public void accountPass() {
+        AccountState as = checker.checkAccount("zxcv1234@gmail.com");
+        assertEquals(as, AccountState.PASS);
+    }
+
+    @Test
+    public void passwordIsNUll() {
+        PasswordState ps = checker.checkPassword("");
+        assertEquals(ps, PasswordState.IS_NULL);
+    }
+    @Test
+    public void passwordIsNotGreteEight() {
+        PasswordState ps = checker.checkPassword("12345");
+        assertEquals(ps, PasswordState.IS_NOT_GREATE_EIGHT);
+    }
+
+    @Test
+    public void passwordIsNotIncludeNumber() {
+        PasswordState ps = checker.checkPassword("zxcvasdasd");
+        assertEquals(ps, PasswordState.IS_NOT_INCLUED_NUMBER);
+    }
+
+    @Test
+    public void passwordIsNotIncludeCapital() {
+        PasswordState ps = checker.checkPassword("zxcvasdasd123");
+        assertEquals(ps, PasswordState.IS_NOT_INCLUDE_CAPITAL);
+    }
+
+    @Test
+    public void passwordPass() {
+        PasswordState ps = checker.checkPassword("Zxcv123456789");
+        assertEquals(ps, PasswordState.PASS);
+    }
+
+}
