abef4ed56df6d72ba71f1fe8e915b8c4720287cf
Momentary <zzser15963@gmail.com>
2020-04-15 14:37:45 +0800

    Initail Commit

---
+++ HW3/app/src/main/java/com/example/hw3/AccountState.java
@@ -0,0 +1,28 @@
+package com.example.hw3;
+
+public enum AccountState {
+    IS_NULL("isNull"), IS_NOT_EMAIL("IsNotEmail"),PASS("pass");
+
+    private String state;
+    private AccountState(String state) {
+        this.state = state;
+    }
+
+    /**
+     *
+     * @param state is status String
+     * @return status is statusEnum object
+     */
+    public static AccountState getStatusEnum(String state) {
+        for (AccountState accountState : AccountState.values()) {
+            if (accountState.getState().equals(state)) {
+                return accountState;
+            }
+        }
+        return null;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+}
