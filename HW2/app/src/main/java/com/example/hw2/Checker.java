74e08bd9a5f53bb74eae10f9ab22da64df1a265b
Momentary <zzser15963@gmail.com>
2020-04-28 16:08:56 +0800

    Fixed HW2 checkstyle error

--- HW2/app/src/main/java/com/example/hw2/Checker.java
+++ HW2/app/src/main/java/com/example/hw2/Checker.java
@@ -2,29 +2,29 @@ package com.example.hw2;
 
 public class Checker {
 
-    AccountState checkAccount(String account){
-        String regex= "^(.+)@(.+)$";
-        if(account.equals("")){
-            return AccountState.IS_NULL;
-        }else if(!account.matches(regex)) {
-            return AccountState.IS_NOT_EMAIL;
-        }
-        return AccountState.PASS;
+  AccountState checkAccount(String account) {
+    String regex = "^(.+)@(.+)$";
+    if (account.equals("")) {
+      return AccountState.IS_NULL;
+    } else if (!account.matches(regex)) {
+      return AccountState.IS_NOT_EMAIL;
     }
+    return AccountState.PASS;
+  }
 
-    PasswordState checkPassword(String password){
-        String regex = "^(.+)[0-9]+.*";
-        String regexCapital = "^(.*)[A-Z]+.*";
-        if(password.equals("")){
-            return PasswordState.IS_NULL;
-        }else if(password.length() <= 8) {
-            return PasswordState.IS_NOT_GREATE_EIGHT;
-        }else if(!password.matches(regex)){
-            return PasswordState.IS_NOT_INCLUED_NUMBER;
-        }else if(!password.matches(regexCapital)){
-            return PasswordState.IS_NOT_INCLUDE_CAPITAL;
-        }
-        return PasswordState.PASS;
+  PasswordState checkPassword(String password) {
+    String regex = "^(.+)[0-9]+.*";
+    String regexCapital = "^(.*)[A-Z]+.*";
+    if (password.equals("")) {
+      return PasswordState.IS_NULL;
+    } else if (password.length() <= 8) {
+      return PasswordState.IS_NOT_GREATE_EIGHT;
+    } else if (!password.matches(regex)) {
+      return PasswordState.IS_NOT_INCLUED_NUMBER;
+    } else if (!password.matches(regexCapital)) {
+      return PasswordState.IS_NOT_INCLUDE_CAPITAL;
     }
+    return PasswordState.PASS;
+  }
 
 }
